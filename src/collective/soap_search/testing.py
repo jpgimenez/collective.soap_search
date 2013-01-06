b490f02e709ab973a2e45fb805d528175597bd34
Juan Pablo Giménez <jpg@rcom.com.ar>
2013-01-06 18:38:19 -0300

    Initial commit

---
+++ src/collective/soap_search/testing.py
@@ -0,0 +1,31 @@
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+
+from plone.testing import z2
+
+from zope.configuration import xmlconfig
+
+
+class Collectivesoap_SearchLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import collective.soap_search
+        xmlconfig.file('configure.zcml', collective.soap_search, context=configurationContext)
+
+        # Install products that use an old-style initialize() function
+        #z2.installProduct(app, 'Products.PloneFormGen')
+
+#    def tearDownZope(self, app):
+#        # Uninstall products installed above
+#        z2.uninstallProduct(app, 'Products.PloneFormGen')
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'collective.soap_search:default')
+
+COLLECTIVE_SOAP_SEARCH_FIXTURE = Collectivesoap_SearchLayer()
+COLLECTIVE_SOAP_SEARCH_INTEGRATION_TESTING = IntegrationTesting(bases=(COLLECTIVE_SOAP_SEARCH_FIXTURE,), name="Collectivesoap_SearchLayer:Integration")
